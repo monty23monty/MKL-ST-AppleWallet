85ef986f50852ccba13f3618bf4d153e8b5e37db
monty23monty <leohenryroberts@gmail.com>
2025-05-27 22:21:57 +0100

    Initial commit

---
+++ src/awsConfig.js
@@ -0,0 +1,12 @@
+export const awsConfig = {
+    Auth: {
+        region: 'eu-west-2',
+        userPoolId: 'eu-west-2_LBU472pes',
+        userPoolWebClientId: '5gg382oqd9sknjnv9fa0hr0km2',
+    },
+    API: {
+        endpoints: [
+            { name: 'wallet', endpoint: 'https://abc123.execute-api.eu-west-2.amazonaws.com' }
+        ]
+    }
+};
