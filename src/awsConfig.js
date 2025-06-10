b09ed3995c5d42ea29c2916af08ac864c8cbe88e
Leo <leohenryroberts@gmail.com>
2025-06-10 14:57:40 +0100

    Updated main and awsConfig.js to use new structure

--- src/awsConfig.js
+++ src/awsConfig.js
@@ -1,14 +1,32 @@
-export const awsConfig = {
+const awsConfig = {
     Auth: {
-        region: 'eu-west-2',
-        userPoolId: 'eu-west-2_LBU472pes',
-        userPoolWebClientId: '5gg382oqd9sknjnv9fa0hr0km2',
+        Cognito: {
+            userPoolId:        'eu-west-2_LBU472pes',
+            userPoolClientId:  '4edcgssa3j6jfeflfrfpq6j62d',
+
+
+
+
+            // optional but silences warnings and lets Amplify know users sign in with e-mail
+            loginWith: {
+                email:    true,
+                username: false,
+                phone:    false,
+            },
+        },
     },
+
+
     API: {
-        endpoints: [
-            { name: 'wallet', endpoint: 'https://abc123.execute-api.eu-west-2.amazonaws.com' }
-        ]
-    }
+        REST: {
+            /** name MUST match the apiName you use in the code below */
+            wallet: {
+                endpoint: 'https://bnlji95zgg.execute-api.eu-west-2.amazonaws.com',
+                region:   'eu-west-2',                // optional, but good practice
+            },
+        },
+    },
 };
+
 
 export default awsConfig;
